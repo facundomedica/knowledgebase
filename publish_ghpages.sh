8856f8a1a0ea24b45da8bd4f522dd3d5d12473d4
Facundo Medica <facundomedica@gmail.com>
2019-10-24 23:47:53 -0300

    fix publish script to add CNAME file

--- publish_ghpages.sh
+++ publish_ghpages.sh
@@ -22,6 +22,8 @@ rm -rf public/*
 echo "Generating site"
 hugo
 
+echo facundo.dev > CNAME
+
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
